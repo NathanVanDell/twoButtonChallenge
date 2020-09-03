eb388551a95d1e0bdd8447783d213bf38609e54d
Nathan Van Dell <nvdski@gmail.com>
2020-09-03 13:33:56 -0700

    Initial Commit

---
+++ twoButtonChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  twoButtonChallenge
+//
+//  Created by Nathan Van Dell on 9/3/20.
+//  Copyright © 2020 Nathan Van Dell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
